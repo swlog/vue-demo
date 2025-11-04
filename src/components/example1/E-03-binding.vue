bc555e03c1c665463478057d16f6ccd543a74e78
swlog <seowonc123@gmail.com>
2025-11-05 01:15:39 +0900

    Initial commit

---
+++ src/components/example1/E-03-binding.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="id" />
+    <input v-model="password" />
+    <p>{{ message }}</p>
+    <button @click="() => {
+      this.message = this.id + ' ' + this.password;
+    }">whoami</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "E03Binding",
+  data() {
+    return {
+      message: "Hello Vue",
+      id: '',
+      password: ''
+    };
+  }
+};
+</script>
---
+++ src/components/example3/ChildComponent.vue
@@ -0,0 +1,15 @@
+ <!-- ChildComponent.vue -->
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <p>{{id}}</p>
+    <p>{{password}} </p>
+    <button @click="$emit('custom-event', 'Hello from child')">Send Event</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message','id','password']
+};
+</script>
