78f2881f476e0ff24756a4771ef39c769ce835a6
swlog <seowonc123@gmail.com>
2025-11-05 01:24:03 +0900

    E01 vue2->vue3

--- src/components/example1/E-01-instance.vue
+++ src/components/example1/E-01-instance.vue
@@ -1,16 +1,33 @@
+<!--<template>-->
+<!--  <div>Hello, {{ message }}</div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "E01Instance",-->
+<!--  data() {-->
+<!--    return {-->
+<!--      message: "Vue!"-->
+<!--    };-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
+
+<!--<style scoped>-->
+<!--div {-->
+<!--  color: blue;-->
+<!--}-->
+<!--</style>-->
+
 <template>
   <div>Hello, {{ message }}</div>
 </template>
 
-<script>
-export default {
-  name: "E01Instance",
-  data() {
-    return {
-      message: "Vue!"
-    };
-  }
-};
+<script setup>
+import { ref } from 'vue'
+
+// 반응형 변수 선언 (기존 data() 역할)
+const message = ref('Vue!')
 </script>
 
 <style scoped>
