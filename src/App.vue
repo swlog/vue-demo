a147a271a06ea942f7dc9736a67af4c089c4d53c
swlog <seowonc123@gmail.com>
2025-11-05 02:06:53 +0900

    E03 vue2->vue3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <E02Reactive/>
+  <E03Binding/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import E02Reactive from "@/components/example1/E-02-reactive.vue";
+import E03Binding from "@/components/example1/E-03-binding.vue";
+
 
 
 
 export default defineComponent({
   name: 'App',
   components: {
-    E02Reactive
+    E03Binding
 
 
   }
--- src/components/example1/E-03-binding.vue
+++ src/components/example1/E-03-binding.vue
@@ -1,23 +1,45 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <input v-model="id" />-->
+<!--    <input v-model="password" />-->
+<!--    <p>{{ message }}</p>-->
+<!--    <button @click="() => {-->
+<!--      this.message = this.id + ' ' + this.password;-->
+<!--    }">whoami</button>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "E03Binding",-->
+<!--  data() {-->
+<!--    return {-->
+<!--      message: "Hello Vue",-->
+<!--      id: '',-->
+<!--      password: ''-->
+<!--    };-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
 <template>
   <div>
     <input v-model="id" />
     <input v-model="password" />
     <p>{{ message }}</p>
-    <button @click="() => {
-      this.message = this.id + ' ' + this.password;
-    }">whoami</button>
+    <button @click="updateMessage">whoami</button>
   </div>
 </template>
 
-<script>
-export default {
-  name: "E03Binding",
-  data() {
-    return {
-      message: "Hello Vue",
-      id: '',
-      password: ''
-    };
-  }
-};
+<script setup>
+import { ref } from 'vue'
+
+// 반응형 변수 정의 (기존 data() 대체)
+const message = ref('Hello Vue')
+const id = ref('')
+const password = ref('')
+
+// 클릭 시 message 업데이트
+const updateMessage = () => {
+  message.value = `${id.value} ${password.value}`
+}
 </script>
